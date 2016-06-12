e51f08940dd7e3aa61146fa63c8be1a50f94644e
Reid Main <reidmain@gmail.com>
2016-06-12 09:25:38 -0700

    Added Roadrunner playground.

---
+++ Roadrunner.playground/Contents.swift
@@ -0,0 +1,10 @@
+/*
+A Playground that will be used as a general scratchpad while working through Apple's iBook on Swift 2.2
+Other more specific Playgrounds will probably be created to test out specific features of Swift and this one will be the most generic one to be used when I don't know where something should go.   
+*/
+
+// Thank god no println or @ symbols.
+print("Hello, world!")
+// Interesting that the first parameter is not named and is variadic. I guess it assumes everything up until a named variable is part of the variadic array. Pretty neat.
+print("Hello, world!", "This is me printing multiple items.", "This is a very nice build in function.", separator: "\n", terminator: "Goodbye")
+
