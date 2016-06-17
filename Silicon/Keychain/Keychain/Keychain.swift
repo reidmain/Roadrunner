00ff6021e518d9c38deddfa615e38cbfda15b73a
Reid Main <reidmain@gmail.com>
2016-06-17 16:07:52 -0700

    Stubbed out the API for Keychain.

--- Silicon/Keychain/Keychain/Keychain.swift
+++ Silicon/Keychain/Keychain/Keychain.swift
@@ -1,9 +1,70 @@
-//
-//  Keychain.swift
-//  Keychain
-//
-//  Created by Reid Main on 2016-06-17.
-//  Copyright © 2016 1414 Degrees. All rights reserved.
-//
-
 import Foundation
+
+enum Accessibility
+{
+	case Everyone
+}
+
+public struct Keychain
+{
+	/**
+	Attempts to retrieve the item from the keychain with the specified key, service and access group.
+	
+	- parameters:
+		- key: The key that the item is associated with.
+		- service: The service that the item is associated with. This is usually the name of the application using the keychain.
+		- accessGroup: The access group that the item is saved to in the keychain. If this parameter is nil the first access group in the application entitlements file is used by default.
+	
+	- returns: The item if it exists in the keychain or nil if they item does not exist in the keychain. If nil is returned an error may have also occurred.
+	*/
+	public static func retrieve<T>(key: String, 
+		service: String, 
+		accessGroup: String? = nil) 
+			-> T?
+	{
+		// TODO: Implement this method.
+		return nil
+	}
+	
+	/**
+	Attempts to save the item to the keychain under the associated key, service and access group with the specified accessibility level.
+	
+	- parameters:
+		- item: The item to be saved to the keychain. The item must adhere to the NSCoding protocol because it will be serialized before it is stored in the keychain. If the item is nil FDKeychain will attempt to delete the item from the keychain instead.
+		- key: The key that the item will be associated with. This parameter must not be nil.
+		- service: The service that the item will be associated with. This is usually the name of the application using the keychain. This parameter must not be nil.
+		- accessGroup: The access group that the item will be saved to in the keychain. If this parameter is nil the first access group in the application entitlements file is used by default.
+		- accessibility: The accessibility level of the item.
+
+	- returns: YES if the item was saved successfully, NO if an error occurred.
+	*/
+	public static func save(item: AnyObject, 
+		key: String, 
+		service: String, 
+		accessGroup: String? = nil, 
+		accessibilityGroup: Int = 0) 
+			-> Bool
+	{
+		// TODO: Implement this method.
+		return false
+	}
+	
+	/**
+	Attempts to delete the item from the keychain with the specified key, service and access group.
+	
+	- parameters:
+		- key: The key that the item is associated with. This parameter must not be nil.
+		- service: The service that the item is associated with. This is usually the name of the application using the keychain. This parameter must not be nil.
+		- accessGroup: The access group that the item is saved to in the keychain. If this parameter is nil the first access group in the application entitlements file is used by default.
+
+	- returns: YES if the item was deleted successfully. NO if an error occurred.
+	*/
+	public static func delete(key: String, 
+		service: String, 
+		accessGroup: String? = nil) 
+			-> Bool
+	{
+		// TODO: Implement this method.
+		return false
+	}
+}
--- Silicon/Keychain/KeychainTests/KeychainTests.swift
+++ Silicon/Keychain/KeychainTests/KeychainTests.swift
@@ -1,36 +1,36 @@
-//
-//  KeychainTests.swift
-//  KeychainTests
-//
-//  Created by Reid Main on 2016-06-17.
-//  Copyright © 2016 1414 Degrees. All rights reserved.
-//
-
 import XCTest
 
 @testable import Keychain
 
 class KeychainTests: XCTestCase
 {
-    override func setUp()
+    func testSaving()
 	{
-        super.setUp()
-    }
-    
-    override func tearDown()
-	{
-        super.tearDown()
-    }
-    
-    func testExample()
-	{
-		XCTAssert(false)
-    }
-    
-    func testPerformanceExample()
-	{
-        self.measureBlock()
-		{
-        }
+		let key = "key";
+		let service = "KeychainTests";
+		let itemInKeychain = "Reid";
+		
+		var item: String? = Keychain.retrieve(key, service: service)
+		
+		XCTAssertNil(item, "No item should exist in the keychain yet.");
+//		XCTAssertNotNil(error, "An error should occur because there is no item in the keychain.");
+//		XCTAssertEqualObjects(error.domain, FDKeychainErrorDomain);
+//		XCTAssertEqual(error.code, errSecItemNotFound);
+		
+		let saveSuccessful = Keychain.save(itemInKeychain, key: key, service: service)
+		
+		XCTAssertTrue(saveSuccessful, "The item should have been successfully saved to the keychain.");
+//		XCTAssertNil(error, "No error should occur while saving the item to the keychain.");
+		
+		item = Keychain.retrieve(key, service: service)
+		
+		XCTAssertNotNil(item, "An item should exist in the keychain.");
+		XCTAssertEqual(item, itemInKeychain);
+//		XCTAssertNil(error, @"No error should occur because there is a valid item in the keychain.");
+		
+		let deleteSuccessful = Keychain.delete(key, service: service)
+		
+		XCTAssertTrue(deleteSuccessful, "The item should have been successfully deleted from the keychain.");
+//		XCTAssertNil(error, @"No error should occur while deleting the item to the keychain.");
     }
 }
