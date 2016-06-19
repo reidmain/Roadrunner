c844a833522aa20e112c8673c7d3969b8a3a6230
Reid Main <reidmain@gmail.com>
2016-06-19 13:49:44 -0700

    Created 'Networking' framework.

---
+++ Silicon/Networking/Networking/Networking.h
@@ -0,0 +1,19 @@
+//
+//  Networking.h
+//  Networking
+//
+//  Created by Reid Main on 2016-06-19.
+//  Copyright © 2016 1414 Degrees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Networking.
+FOUNDATION_EXPORT double NetworkingVersionNumber;
+
+//! Project version string for Networking.
+FOUNDATION_EXPORT const unsigned char NetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networking/PublicHeader.h>
+
+
