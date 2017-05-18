6c6cb394f814eff60df51935b0a5533684f4424c
Reid Main <reidmain@gmail.com>
2017-05-17 21:47:13 -0700

    Created Logging project.

---
+++ Silicon/Logging/Logging/Logging.h
@@ -0,0 +1,19 @@
+//
+//  Logging.h
+//  Logging
+//
+//  Created by Reid Main on 5/17/17.
+//  Copyright © 2017 Reid Main. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Logging.
+FOUNDATION_EXPORT double LoggingVersionNumber;
+
+//! Project version string for Logging.
+FOUNDATION_EXPORT const unsigned char LoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logging/PublicHeader.h>
+
+
