bfd566f217afb16772e5e7d786e0aa1313ae5ad4
Reid Main <reidmain@gmail.com>
2017-05-22 22:16:47 -0700

    Added watchOS target.

--- Silicon/Logging/Logging/Logging.h
+++ Silicon/Logging/Logging/Logging.h
@@ -5,5 +5,3 @@ FOUNDATION_EXPORT double LoggingVersionNumber;
 
 //! Project version string for Logging.
 FOUNDATION_EXPORT const unsigned char LoggingVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Logging/PublicHeader.h>
